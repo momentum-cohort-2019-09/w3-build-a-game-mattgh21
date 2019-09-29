991e52fa841c169699fbe39ac121f92f5579db47
Matt Gouchoe-Hanas <mattgh21@gmail.com>
2019-09-28 22:46:27 -0400

    adds ability to move player around board, but cannot move player to the middle

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-//square.clearRect(160, 160, 60, 60)
-
 class Game {
     constructor(canvasID) {
         const canvas = document.getElementById(canvasID)
         this.square = canvas.getContext('2d')
         this.size = { x: canvas.width, y: canvas.height }
 
+        this.keyboarder = new Keyboarder()
+
         // this.fillBackground()
 
-        this.player_position = {
-            x: 160,
-            y: 160
-        }
+        this.player_position = { x: 225, y: 225 }
+
+
 
         this.player_size = {
-            width: 60,
-            height: 60
+            width: 50,
+            height: 50
         }
 
         this.goldenSnitch_position = {
@@ -52,6 +51,26 @@ class Game {
         this.player.draw(this.square)
     }
 
+    move_snitch(new_position) {
+        this.goldenSnitch.position = new_position
+        this.goldenSnitch.draw(this.square)
+    }
+
+
+    run() {
+        const tick = () => {
+            console.log('hi')
+            this.update()
+            this.player.update(this)
+
+
+            requestAnimationFrame(tick)
+
+        }
+        tick()
+    }
+
+
 
 }
 
@@ -60,7 +79,44 @@ class Player {
     constructor(position, size) {
         this.position = position
         this.size = size
+
+
     }
+
+    update(game) {
+        if (game.keyboarder.isDown(Keyboarder.KEYS.LEFT)) {
+            if (this.position.x === Player.LOCATIONS.MIDDLE_RIGHT.x) {
+                this.position.x = Player.LOCATIONS.TOP_MIDDLE.x
+            } else {
+                this.position.x = Player.LOCATIONS.MIDDLE_LEFT.x
+            }
+
+
+        }
+        if (game.keyboarder.isDown(Keyboarder.KEYS.RIGHT)) {
+
+            this.position.x = Player.LOCATIONS.MIDDLE_RIGHT.x
+
+        }
+        if (game.keyboarder.isDown(Keyboarder.KEYS.UP)) {
+            console.log('move up ,' + Player.LOCATIONS.MIDDLE_MIDDLE)
+            this.position.y = Player.LOCATIONS.TOP_MIDDLE.y
+
+        }
+        if (game.keyboarder.isDown(Keyboarder.KEYS.DOWN)) {
+            console.log('move down ,' + Player.LOCATIONS.MIDDLE_MIDDLE)
+            this.position.y = Player.LOCATIONS.BOTTOM_MIDDLE.y
+        }
+
+        if (game.keyboarder.isDown(Keyboarder.KEYS.S)) {
+            console.log(Player.LOCATIONS.MIDDLE_MIDDLE.x)
+            this.position = Player.LOCATIONS.MIDDLE_MIDDLE
+        }
+
+
+        this.draw(game.square)
+    }
+
     draw(screen) {
         // screen.clearRect(0, 0, this.size.width, this.size.height)
         screen.fillStyle = "white";
@@ -78,6 +134,20 @@ class Player {
         this.draw(screen)
 
     }
+
+    // if (game.keyboarder.isDown(Keyboarder.KEYS.LEFT))
+
+}
+
+Player.LOCATIONS = {
+    MIDDLE_MIDDLE: { x: 225, y: 225 },
+    TOP_MIDDLE: { x: 225, y: 160 },
+    BOTTOM_MIDDLE: { x: 225, y: 285 },
+    MIDDLE_LEFT: { x: 160, y: 225 },
+    MIDDLE_RIGHT: { x: 285, y: 225 }
+
+
+
 }
 
 class GoldenSnitch {
@@ -95,7 +165,30 @@ class GoldenSnitch {
             this.size.height
         )
     }
+
+    //     move(new_snicthPosition, screen) {
+    //         this.position.x = new_snitchPosition.x
+    //         this.position.y = new_snitchPosition.y
+    //         this.draw(screen)
+
+    // }
 }
+
+
+
+
+function colliding(b1, b2) {
+    return !(
+        b1 === b2 ||
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+        b1.center.x + b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2
+    )
+}
+
+
+
 
 
 // box.draw(square)
@@ -112,11 +205,4 @@ class GoldenSnitch {
 
 let game = new Game('canvas')
 
-new_position = {
-    x: 250,
-    y: 250
-}
-
-game.move_player(new_position)
-
-game.update()
+game.run()
